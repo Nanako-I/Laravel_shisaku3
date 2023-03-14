771a80fbea48f4b394ccc85dafd94794e827e783
Nanako-I <milklmi.1103775@gmail.com>
2023-03-14 14:45:44 +0000

    html追加

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -8,8 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Person extends Model
 {
     use HasFactory;
-    protected $fillable = ['person_name','date_of_birth' , 'age','gender','profile_image','disability_name'];
     protected $table = 'people';
+    protected $fillable = ['person_name','date_of_birth' , 'age','gender','profile_image','disability_name'];
+    
 }
 
 // protected $fillable = ['person_name','date_of_birth' , 'age','gender','profile_image','disability_name'];
--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,8 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    
+    protected $table = 'users';
 
     /**
      * The attributes that are mass assignable.
@@ -23,7 +25,7 @@ class User extends Authenticatable
         'password',
     ];
     
-    protected $table = 'users';
+    
     
     /**
      * The attributes that should be hidden for serialization.
